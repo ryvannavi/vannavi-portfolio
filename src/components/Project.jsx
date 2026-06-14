1bb5a8a559c62ff4e2770dbd3fff8add0ffe29d4
Navi <vannaviryy@gmail.com>
2026-06-14 14:33:47 +0700

    Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -27,7 +27,7 @@ function Project() {
       technologies: ['Laravel', 'PHP', 'SQLite', 'Tailwind CSS', 'Groq AI', 'AlpineJS'],
       featured: false,
       githubLink: 'https://github.com/ryvannavi/novasupport',
-      liveLink: null,
+      liveLink: 'https://novasupport-7iva.onrender.com/',
       class: 'FYP 401 001 - Final Year Project - I'
     }
   ];
